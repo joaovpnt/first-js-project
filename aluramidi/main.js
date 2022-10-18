44d6f071eddbb10fd8a32a4d460b3a355dd867ac
joaovpnt <55442431+joaovpnt@users.noreply.github.com>
2022-10-17 23:23:22 -0300

    Atualizações no main.js
    Eventos no Teclado + Atualização lógica

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,28 +1,41 @@
-function tocaSom(idElementoAudio){
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-    document.querySelector(idElementoAudio).play();
+    if (elemento === null) {
+        alert('Elemento não encontrado')
+    }
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else {
+        console.log('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-
-    const instrumento = tecla.classList[1]
-
+    const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    console.log(idAudio);
-
-    tecla.onclick = function(){
-        tocaSom(idAudio)
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
 
-    contador  = contador + 1;
+    tecla.onkeydown = function(evento){
 
-    console.log(contador);
+        console.log(evento.code == 'Space');
+
+        if ((evento.code === 'Space' || evento.code === 'Enter')){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 
 }
